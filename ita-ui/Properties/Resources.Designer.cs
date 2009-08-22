5c72c8235ae4ef93d6c6152920f9c6b494ce3b46
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2009-08-22 11:42:22 +0000

    Changed namespace.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@89 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/Properties/Resources.Designer.cs
+++ ita-ui/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ita_ui.Properties {
+namespace iTunesAgent.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ita_ui.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ita_ui.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("iTunesAgent.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
