8b0a7c430ba9c9dd56eadab6ca95aac6b18f828a
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2009-08-30 11:08:49 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@97 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-media-connectors-tests/WindowsPortableDevicesServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesAgent.Connectors
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WindowsPortableDevicesServiceTest
+    {
+
+        [Test]
+        public void TestEnumerate()
+        {
+            WindowsPortableDevicesService s = new WindowsPortableDevicesService();
+            s.Enumerate();
+        }
+    }
+}
