3ac56995c887d4dc2b62c727bd543594b0f92b1b
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2009-11-14 14:45:18 +0000

    Modified layout and design.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@107 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/MainForm.Designer.cs
+++ ita-ui/MainForm.Designer.cs
@@ -46,9 +46,11 @@
             this.panMainPanel = new System.Windows.Forms.Panel();
             this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.panButtons = new System.Windows.Forms.Panel();
-            this.btnMyDevices = new System.Windows.Forms.Button();
-            this.panViewPlaceholder = new System.Windows.Forms.Panel();
             this.btnPreferences = new System.Windows.Forms.Button();
+            this.btnMyDevices = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.panViewPlaceholder = new System.Windows.Forms.Panel();
+            this.toolTipMain = new System.Windows.Forms.ToolTip(this.components);
             this.trayContextMenu.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.panMainPanel.SuspendLayout();
@@ -76,37 +78,37 @@
             this.tssTraySeparator2,
             this.tssTraySynchronize});
             this.trayContextMenu.Name = "trayContextMenu";
-            this.trayContextMenu.Size = new System.Drawing.Size(156, 104);
+            this.trayContextMenu.Size = new System.Drawing.Size(145, 104);
             // 
             // miTrayExit
             // 
             this.miTrayExit.BackColor = System.Drawing.SystemColors.Control;
             this.miTrayExit.Name = "miTrayExit";
-            this.miTrayExit.Size = new System.Drawing.Size(155, 22);
+            this.miTrayExit.Size = new System.Drawing.Size(144, 22);
             this.miTrayExit.Text = "E&xit";
             this.miTrayExit.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
             // 
             // tssTraySeparator1
             // 
             this.tssTraySeparator1.Name = "tssTraySeparator1";
-            this.tssTraySeparator1.Size = new System.Drawing.Size(152, 6);
+            this.tssTraySeparator1.Size = new System.Drawing.Size(141, 6);
             // 
             // tssTrayMyDevices
             // 
             this.tssTrayMyDevices.Name = "tssTrayMyDevices";
-            this.tssTrayMyDevices.Size = new System.Drawing.Size(155, 22);
+            this.tssTrayMyDevices.Size = new System.Drawing.Size(144, 22);
             this.tssTrayMyDevices.Text = "My devices...";
             // 
             // tssTrayPreferences
             // 
             this.tssTrayPreferences.Name = "tssTrayPreferences";
-            this.tssTrayPreferences.Size = new System.Drawing.Size(155, 22);
+            this.tssTrayPreferences.Size = new System.Drawing.Size(144, 22);
             this.tssTrayPreferences.Text = "Preferences...";
             // 
             // tssTraySeparator2
             // 
             this.tssTraySeparator2.Name = "tssTraySeparator2";
-            this.tssTraySeparator2.Size = new System.Drawing.Size(152, 6);
+            this.tssTraySeparator2.Size = new System.Drawing.Size(141, 6);
             // 
             // tssTraySynchronize
             // 
@@ -115,40 +117,41 @@
             this.tssSynchronizeSeparator1,
             this.tssSynchronizeNoDevices});
             this.tssTraySynchronize.Name = "tssTraySynchronize";
-            this.tssTraySynchronize.Size = new System.Drawing.Size(155, 22);
+            this.tssTraySynchronize.Size = new System.Drawing.Size(144, 22);
             this.tssTraySynchronize.Text = "Synchronize";
             // 
             // tssSynchronizeAll
             // 
             this.tssSynchronizeAll.Name = "tssSynchronizeAll";
-            this.tssSynchronizeAll.Size = new System.Drawing.Size(137, 22);
+            this.tssSynchronizeAll.Size = new System.Drawing.Size(132, 22);
             this.tssSynchronizeAll.Text = "All devices";
             // 
             // tssSynchronizeSeparator1
             // 
             this.tssSynchronizeSeparator1.Name = "tssSynchronizeSeparator1";
-            this.tssSynchronizeSeparator1.Size = new System.Drawing.Size(134, 6);
+            this.tssSynchronizeSeparator1.Size = new System.Drawing.Size(129, 6);
             // 
             // tssSynchronizeNoDevices
             // 
             this.tssSynchronizeNoDevices.Enabled = false;
             this.tssSynchronizeNoDevices.Name = "tssSynchronizeNoDevices";
-            this.tssSynchronizeNoDevices.Size = new System.Drawing.Size(137, 22);
+            this.tssSynchronizeNoDevices.Size = new System.Drawing.Size(132, 22);
             this.tssSynchronizeNoDevices.Text = "No devices";
             // 
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tslITunesStatus});
-            this.statusStrip.Location = new System.Drawing.Point(0, 406);
+            this.statusStrip.Location = new System.Drawing.Point(0, 400);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(679, 22);
+            this.statusStrip.Size = new System.Drawing.Size(674, 22);
+            this.statusStrip.SizingGrip = false;
             this.statusStrip.TabIndex = 3;
             // 
             // tslITunesStatus
             // 
             this.tslITunesStatus.Name = "tslITunesStatus";
-            this.tslITunesStatus.Size = new System.Drawing.Size(664, 17);
+            this.tslITunesStatus.Size = new System.Drawing.Size(649, 17);
             this.tslITunesStatus.Spring = true;
             this.tslITunesStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
@@ -159,7 +162,7 @@
             this.panMainPanel.Location = new System.Drawing.Point(0, 0);
             this.panMainPanel.Name = "panMainPanel";
             this.panMainPanel.Padding = new System.Windows.Forms.Padding(5);
-            this.panMainPanel.Size = new System.Drawing.Size(679, 406);
+            this.panMainPanel.Size = new System.Drawing.Size(674, 400);
             this.panMainPanel.TabIndex = 4;
             // 
             // splitContainer
@@ -176,79 +179,110 @@
             // splitContainer.Panel2
             // 
             this.splitContainer.Panel2.Controls.Add(this.panViewPlaceholder);
-            this.splitContainer.Size = new System.Drawing.Size(669, 396);
+            this.splitContainer.Size = new System.Drawing.Size(664, 390);
             this.splitContainer.SplitterDistance = 92;
             this.splitContainer.TabIndex = 5;
             // 
             // panButtons
             // 
             this.panButtons.AutoSize = true;
-            this.panButtons.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panButtons.BackColor = System.Drawing.Color.White;
             this.panButtons.Controls.Add(this.btnPreferences);
             this.panButtons.Controls.Add(this.btnMyDevices);
+            this.panButtons.Controls.Add(this.buttonHome);
             this.panButtons.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panButtons.Location = new System.Drawing.Point(0, 0);
             this.panButtons.Name = "panButtons";
-            this.panButtons.Size = new System.Drawing.Size(92, 396);
+            this.panButtons.Size = new System.Drawing.Size(92, 390);
             this.panButtons.TabIndex = 5;
+            // 
+            // btnPreferences
+            // 
+            this.btnPreferences.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPreferences.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPreferences.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPreferences.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPreferences.FlatAppearance.BorderSize = 0;
+            this.btnPreferences.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnPreferences.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.btnPreferences.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPreferences.Image = global::iTunesAgent.UI.Properties.Resources.preferences;
+            this.btnPreferences.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnPreferences.Location = new System.Drawing.Point(0, 130);
+            this.btnPreferences.Name = "btnPreferences";
+            this.btnPreferences.Size = new System.Drawing.Size(92, 64);
+            this.btnPreferences.TabIndex = 5;
+            this.btnPreferences.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolTipMain.SetToolTip(this.btnPreferences, "Application preferences");
+            this.btnPreferences.UseVisualStyleBackColor = true;
+            this.btnPreferences.Click += new System.EventHandler(this.btnPreferences_Click);
             // 
             // btnMyDevices
             // 
             this.btnMyDevices.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnMyDevices.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMyDevices.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnMyDevices.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMyDevices.FlatAppearance.BorderSize = 0;
             this.btnMyDevices.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.btnMyDevices.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnMyDevices.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.btnMyDevices.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMyDevices.Image = global::iTunesAgent.UI.Properties.Resources.devices_icon;
+            this.btnMyDevices.Image = global::iTunesAgent.UI.Properties.Resources.media_players;
             this.btnMyDevices.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnMyDevices.Location = new System.Drawing.Point(0, 0);
+            this.btnMyDevices.Location = new System.Drawing.Point(0, 65);
             this.btnMyDevices.Name = "btnMyDevices";
-            this.btnMyDevices.Size = new System.Drawing.Size(92, 64);
-            this.btnMyDevices.TabIndex = 0;
-            this.btnMyDevices.Text = "My Devices";
+            this.btnMyDevices.Size = new System.Drawing.Size(92, 65);
+            this.btnMyDevices.TabIndex = 4;
             this.btnMyDevices.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnMyDevices.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolTipMain.SetToolTip(this.btnMyDevices, "Manage my devices");
             this.btnMyDevices.UseVisualStyleBackColor = true;
             this.btnMyDevices.Click += new System.EventHandler(this.btnMyDevices_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonHome.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonHome.FlatAppearance.BorderSize = 0;
+            this.buttonHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.buttonHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.Image = global::iTunesAgent.UI.Properties.Resources.home;
+            this.buttonHome.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonHome.Location = new System.Drawing.Point(0, 0);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(92, 65);
+            this.buttonHome.TabIndex = 2;
+            this.buttonHome.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonHome.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.toolTipMain.SetToolTip(this.buttonHome, "Home screen");
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
             // 
             // panViewPlaceholder
             // 
             this.panViewPlaceholder.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panViewPlaceholder.Location = new System.Drawing.Point(0, 0);
             this.panViewPlaceholder.Name = "panViewPlaceholder";
-            this.panViewPlaceholder.Size = new System.Drawing.Size(573, 396);
+            this.panViewPlaceholder.Size = new System.Drawing.Size(568, 390);
             this.panViewPlaceholder.TabIndex = 6;
-            // 
-            // btnPreferences
-            // 
-            this.btnPreferences.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnPreferences.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnPreferences.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnPreferences.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.btnPreferences.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btnPreferences.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPreferences.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnPreferences.Location = new System.Drawing.Point(0, 64);
-            this.btnPreferences.Name = "btnPreferences";
-            this.btnPreferences.Size = new System.Drawing.Size(92, 64);
-            this.btnPreferences.TabIndex = 1;
-            this.btnPreferences.Text = "Preferences";
-            this.btnPreferences.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnPreferences.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnPreferences.UseVisualStyleBackColor = true;
-            this.btnPreferences.Click += new System.EventHandler(this.btnPreferences_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 428);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(674, 422);
             this.Controls.Add(this.panMainPanel);
             this.Controls.Add(this.statusStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(620, 430);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(680, 450);
             this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "iTunes Agent";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.trayContextMenu.ResumeLayout(false);
@@ -275,7 +309,6 @@
         private System.Windows.Forms.SplitContainer splitContainer;
         private System.Windows.Forms.Panel panViewPlaceholder;
         private System.Windows.Forms.Panel panButtons;
-        private System.Windows.Forms.Button btnMyDevices;
         private System.Windows.Forms.ToolStripStatusLabel tslITunesStatus;
         private System.Windows.Forms.ToolStripSeparator tssTraySeparator1;
         private System.Windows.Forms.ToolStripMenuItem tssTrayMyDevices;
@@ -285,7 +318,10 @@
         private System.Windows.Forms.ToolStripMenuItem tssSynchronizeAll;
         private System.Windows.Forms.ToolStripSeparator tssSynchronizeSeparator1;
         private System.Windows.Forms.ToolStripMenuItem tssSynchronizeNoDevices;
+        private System.Windows.Forms.Button buttonHome;
         private System.Windows.Forms.Button btnPreferences;
+        private System.Windows.Forms.Button btnMyDevices;
+        private System.Windows.Forms.ToolTip toolTipMain;
     }
 }
 
