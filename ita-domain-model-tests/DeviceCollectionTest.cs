55e22bd5ee0d40715eaba338f4172f50e40bec9a
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2009-11-16 19:40:07 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@115 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-domain-model-tests/DeviceCollectionTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using NUnit.Framework;
+
+namespace iTunesAgent.Domain
+{
+
+    [TestFixture]
+    public class DeviceCollectionTest
+    {
+
+        [Test]
+        public void TestSerializeDeviceCollection()
+        {
+            DeviceCollection collection = new DeviceCollection();
+
+            MassStorageDevice msd = new MassStorageDevice();
+            msd.Name = "Test1";
+            msd.Identifier = "file:mydevice.id";
+            msd.AddPlaylistAssociation(1234, "pattern", "Playlist1");
+            msd.AddPlaylistAssociation(4567, "pattern", "Playlist2");
+
+            collection.Devices.Add(msd);
+
+            WPDDevice wpd = new WPDDevice();
+            wpd.Name = "Test2";
+            wpd.Identifier = "usb:1234567890";
+            wpd.AddPlaylistAssociation(4321, "pattern2", "Playlist3");
+            wpd.AddPlaylistAssociation(7654, "pattern2", "Playlist4");
+
+            collection.Devices.Add(wpd);
+
+            XmlSerializer serializer = new XmlSerializer(collection.GetType());
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream("test.xml", FileMode.Create);
+                serializer.Serialize(fs, collection);
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+
+            FileStream read = null;
+            try
+            {
+                read = new FileStream("test.xml", FileMode.Open);
+                DeviceCollection deserializedCollection =
+                    (DeviceCollection)serializer.Deserialize(read);
+                
+                Assert.NotNull(deserializedCollection);
+                Assert.AreEqual(2, deserializedCollection.Devices.Count);
+
+                Device d1 = deserializedCollection.Devices[0];
+                Device d2 = deserializedCollection.Devices[1];
+
+                Assert.AreEqual("Test1", d1.Name);
+                Assert.AreEqual("file:mydevice.id", d1.Identifier);
+                Assert.AreEqual(2, d1.Playlists.Count);
+                Assert.AreEqual(1234, d1.Playlists[0].PlaylistID);
+                Assert.AreEqual("pattern", d1.Playlists[0].PatternID);
+                Assert.AreEqual("Playlist1", d1.Playlists[0].DeviceFolder);
+                Assert.AreEqual(4567, d1.Playlists[1].PlaylistID);
+                Assert.AreEqual("pattern", d1.Playlists[1].PatternID);
+                Assert.AreEqual("Playlist2", d1.Playlists[1].DeviceFolder);
+
+                Assert.AreEqual("Test2", d2.Name);
+                Assert.AreEqual("usb:1234567890", d2.Identifier);
+                Assert.AreEqual(2, d2.Playlists.Count);
+                Assert.AreEqual(4321, d2.Playlists[0].PlaylistID);
+                Assert.AreEqual("pattern2", d2.Playlists[0].PatternID);
+                Assert.AreEqual("Playlist3", d2.Playlists[0].DeviceFolder);
+                Assert.AreEqual(7654, d2.Playlists[1].PlaylistID);
+                Assert.AreEqual("pattern2", d2.Playlists[1].PatternID);
+                Assert.AreEqual("Playlist4", d2.Playlists[1].DeviceFolder);
+
+
+            }
+            finally
+            {
+                if (read != null)
+                {
+                    read.Close();
+                }
+            }
+
+
+
+           File.Delete("test.xml");
+        }
+
+    }
+}
