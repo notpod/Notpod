26c3a51dfe14e338f2c9d13d1b07e019acbb27c6
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2010-04-03 12:26:38 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@180 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-itunes/ITunesConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+namespace iTunesAgent.Services.iTunes
+{
+    /*!
+     * Factory for creating connections to iTunes.
+     * \sa AbstractMediaSoftwareServiceFactory
+     */
+    public class ITunesConnectionFactory : AbstractMediaSoftwareConnectionFactory
+    {
+        public override object CreateMediaSoftwareConnection()
+        {
+            return new iTunesAppClass();
+        }
+    }
+}
