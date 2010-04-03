e6ac0066875081458b6dae99768d26e1c14259ff
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2010-04-03 12:37:56 +0000

    Added logging.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@191 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/PreferencesPanel.cs
+++ ita-ui/PreferencesPanel.cs
@@ -6,14 +6,24 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using log4net;
 
 namespace iTunesAgent.UI
 {
     public partial class PreferencesPanel : UserControl
     {
+
+        private ILog l = LogManager.GetLogger(typeof(PreferencesPanel));
+
         public PreferencesPanel()
         {
             InitializeComponent();
+        }
+
+        private void PreferencesPanel_Load(object sender, EventArgs e)
+        {            
+            l.Debug("Loading PreferencesPanel");
+            
         }
     }
 }
