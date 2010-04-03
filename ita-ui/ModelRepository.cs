1358e01af2b6c6fcc80db1605bf4deaa7fcdf6c7
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2010-04-03 12:46:38 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@198 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-ui/ModelRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using iTunesAgent.Domain;
+using log4net;
+
+namespace iTunesAgent.UI
+{
+    public class ModelRepository : Dictionary<string, object>
+    {
+        ILog l = LogManager.GetLogger(typeof(ModelRepository));
+
+        /// <summary>
+        /// Deserialize a component and add it to the repository.
+        /// </summary>
+        /// <param name="component">Component ID.</param>
+        /// <param name="type">Component type.</param>
+        /// <param name="stream">Stream to deserialize from.</param>
+        public void Deserialize(string component, Type type, Stream stream)
+        {            
+            XmlSerializer ds = new XmlSerializer(type);
+            Object deserialized = ds.Deserialize(stream);
+
+            this.Add(component, deserialized);
+        }
+
+        /// <summary>
+        /// Serialize a component.
+        /// </summary>
+        /// <param name="component"></param>
+        /// <param name="type"></param>
+        /// <param name="stream"></param>
+        public void Serialize(string component, Type type, Stream stream)
+        {
+            XmlSerializer xs = new XmlSerializer(type);
+            xs.Serialize(stream, this[component]);
+        }
+
+    }
+}
