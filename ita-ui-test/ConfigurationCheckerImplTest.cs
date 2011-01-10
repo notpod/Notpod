55a5c1a32635a322a469ee0dcc4186bfdb72c4ec
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-01-10 20:18:55 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@241 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-ui-test/ConfigurationCheckerImplTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using Rhino.Mocks;
+
+namespace iTunesAgent.UI.Configuration
+{
+    [TestFixture]
+    public class ConfigurationCheckerImplTest
+    {
+        private string testConfigurationFile = "testConfiguration.xml";
+
+        MockRepository mock = new MockRepository();
+        ConfigurationWriter configurationWriter = null;
+
+        [TestFixtureSetUp]
+        public void Before()
+        {
+            if (File.Exists(testConfigurationFile))
+            {
+                File.Delete(testConfigurationFile);
+            }
+
+            SetUpMock();
+        }
+
+        private void SetUpMock()
+        {
+            configurationWriter = mock.StrictMock<ConfigurationWriter>();
+        }
+
+        [Test]
+        public void CheckConfigurationShouldCallWriteOnConfigurationManagerWhenNoExistingConfiguration()
+        {
+            ConfigurationCheckerImpl checker = new ConfigurationCheckerImpl();
+            checker.ConfigurationWriter = configurationWriter;            
+            Expect.Call(configurationWriter.Write);
+            mock.ReplayAll();
+
+            checker.CheckConfiguration();
+
+            mock.VerifyAll();
+        }
+    }
+}
---
+++ ita-ui-test/DefaultConfigurationToFileWriterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using iTunesAgent.UI.Properties;
+
+namespace iTunesAgent.UI.Configuration
+{
+    [TestFixture]
+    public class DefaultConfigurationToFileWriterTest
+    {
+        private string testConfigurationFileName = "testConfiguation.xml";
+
+        [TestFixtureSetUp]
+        public void Before()
+        {
+            if (File.Exists(testConfigurationFileName))
+            {
+                File.Delete(testConfigurationFileName);
+            }
+        }
+                
+        [Test]
+        public void TestWrite()
+        {
+
+            DefaultConfigurationToFileWriter configurationWriter = new DefaultConfigurationToFileWriter(testConfigurationFileName);
+            configurationWriter.Write();
+
+            Assert.AreEqual(true, File.Exists(testConfigurationFileName));
+
+            string writtenContent = File.ReadAllText(testConfigurationFileName);
+            Assert.AreEqual(DefaultConfigurationToFileWriter.GetDefaultConfiguration(), writtenContent);
+        }
+    }
+}
