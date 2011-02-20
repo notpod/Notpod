ca8788218c041c2304ec50f81b7873c8787cfe8c
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-02-20 19:17:40 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@256 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-media-connectors/CompatibleDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesAgent.Connectors
+{
+    public abstract class CompatibleDevice
+    {
+        private string identifier;
+
+        private string name;
+
+        public string Identifier
+        {
+            get { return identifier; }
+            set { identifier = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ ita-media-connectors/WindowsPortableDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesAgent.Connectors.Domain
+{
+    /// <summary>
+    /// Value object for storing information about a compatible portable device connected to the computer.
+    /// </summary>
+    public class WindowsPortableDevice : CompatibleDevice
+    {
+
+    }
+}
