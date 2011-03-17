fe3a869e3dfbe3a4f387a9b1a829b16cac8a8c77
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-03-17 19:41:59 +0000

    Added methods for connecting and disconnecting and populating values from the device.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@265 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-media-connectors/WindowsPortableDevice.cs
+++ ita-media-connectors/WindowsPortableDevice.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PortableDeviceApiLib;
 
 namespace iTunesAgent.Connectors.Domain
 {
@@ -10,6 +11,64 @@ namespace iTunesAgent.Connectors.Domain
     /// </summary>
     public class WindowsPortableDevice : CompatibleDevice
     {
+        private string deviceId;
 
+        private PortableDeviceClass portableDevice;
+
+        private bool isConnected = false;
+
+        public WindowsPortableDevice(string deviceId)
+        {
+            this.deviceId = deviceId;
+            portableDevice = new PortableDeviceClass();
+        }
+
+        public void Connect(string applicationName, float majorVersion, float minorVersion)
+        {
+            //Creating propValues for connection
+            IPortableDeviceValues clientValues = (IPortableDeviceValues)new PortableDeviceTypesLib.PortableDeviceValuesClass();
+
+            //Set the application name
+            _tagpropertykey prop = PortableDevicePKeys.WPD_CLIENT_NAME;
+            clientValues.SetStringValue(ref prop, applicationName);
+            //Set the App version
+            prop = PortableDevicePKeys.WPD_CLIENT_MAJOR_VERSION;
+            clientValues.SetFloatValue(ref prop, majorVersion);
+            //Set the app minor version
+            prop = PortableDevicePKeys.WPD_CLIENT_MINOR_VERSION;
+            clientValues.SetFloatValue(ref prop, minorVersion);
+
+            //Open connection
+            this.portableDevice.Open(this.deviceId, clientValues);
+            
+            SetValues();
+
+            this.isConnected = true;           
+        }
+
+        private void SetValues()
+        {
+            IPortableDeviceContent content;
+            IPortableDeviceProperties properties;
+            PortableDeviceApiLib.IPortableDeviceValues propertyValues;
+
+            this.portableDevice.Content(out content);
+            content.Properties(out properties);
+
+            properties.GetValues("DEVICE", null, out propertyValues);
+
+            string val = string.Empty;
+            propertyValues.GetStringValue(ref PortableDevicePKeys.WPD_DEVICE_FRIENDLY_NAME, out val);
+            this.Name = val;
+
+            
+        }
+
+        public void Disconnect()
+        {
+
+            this.portableDevice.Close();
+            isConnected = false;
+        }
     }
 }
