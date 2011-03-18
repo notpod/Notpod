941af843b5be3c83675ae60e905b92b560a8bd4a
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-03-18 20:53:49 +0000

    Added test. Changed target framework.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@282 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-ui-test/Components/Wizard/WizardFormTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+namespace iTunesAgent.UI.Components.Wizard
+{
+    [TestFixture]
+    public class WizardFormTest
+    {
+        MockRepository mock = new MockRepository();
+
+        [Test]
+        [ExpectedException(typeof(NoWizardPagesException))]
+        public void StartWizard_WhenNoPagesAdded_ShouldThrowException()
+        {
+            WizardForm form = new WizardForm();
+            form.StartWizard(null);
+        }
+                
+    }
+}
