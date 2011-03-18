e6d82d66ed68b0877c5a1f96b3cb6281f9e734e0
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-03-18 20:48:34 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@272 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ ita-ui/Components/Wizard/NoWizardPagesException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesAgent.UI.Components.Wizard
+{
+    public class NoWizardPagesException : Exception
+    {
+        public NoWizardPagesException() : base() 
+        {
+                        
+        }
+
+        public NoWizardPagesException(string message) : base(message)
+        {
+            
+        }
+    }
+}
