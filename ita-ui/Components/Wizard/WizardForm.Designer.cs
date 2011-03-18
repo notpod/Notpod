68033912709f9db582261fd72e45051de0c83308
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-03-18 20:49:41 +0000

    Added button control handling and forward moving.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@274 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/Components/Wizard/WizardForm.Designer.cs
+++ ita-ui/Components/Wizard/WizardForm.Designer.cs
@@ -31,13 +31,17 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.wizardContentPanel = new System.Windows.Forms.TableLayoutPanel();
             this.buttonsPanel = new System.Windows.Forms.Panel();
-            this.buttonBack = new System.Windows.Forms.Button();
-            this.buttonNext = new System.Windows.Forms.Button();
-            this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonFinish = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.labelPageTitle = new System.Windows.Forms.Label();
+            this.panelWizardPageContainer = new System.Windows.Forms.Panel();
             this.mainPanel.SuspendLayout();
             this.wizardContentPanel.SuspendLayout();
             this.buttonsPanel.SuspendLayout();
+            this.panelHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -46,7 +50,7 @@
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(564, 396);
+            this.mainPanel.Size = new System.Drawing.Size(562, 436);
             this.mainPanel.TabIndex = 0;
             // 
             // wizardContentPanel
@@ -54,13 +58,15 @@
             this.wizardContentPanel.ColumnCount = 1;
             this.wizardContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.wizardContentPanel.Controls.Add(this.buttonsPanel, 0, 2);
+            this.wizardContentPanel.Controls.Add(this.panelHeader, 0, 0);
+            this.wizardContentPanel.Controls.Add(this.panelWizardPageContainer, 0, 1);
             this.wizardContentPanel.Location = new System.Drawing.Point(3, 3);
             this.wizardContentPanel.Name = "wizardContentPanel";
             this.wizardContentPanel.RowCount = 3;
             this.wizardContentPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.wizardContentPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.wizardContentPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.wizardContentPanel.Size = new System.Drawing.Size(558, 390);
+            this.wizardContentPanel.Size = new System.Drawing.Size(557, 433);
             this.wizardContentPanel.TabIndex = 0;
             // 
             // buttonsPanel
@@ -70,10 +76,40 @@
             this.buttonsPanel.Controls.Add(this.buttonNext);
             this.buttonsPanel.Controls.Add(this.buttonBack);
             this.buttonsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonsPanel.Location = new System.Drawing.Point(3, 343);
+            this.buttonsPanel.Location = new System.Drawing.Point(3, 386);
             this.buttonsPanel.Name = "buttonsPanel";
-            this.buttonsPanel.Size = new System.Drawing.Size(552, 44);
+            this.buttonsPanel.Size = new System.Drawing.Size(551, 44);
             this.buttonsPanel.TabIndex = 0;
+            // 
+            // buttonFinish
+            // 
+            this.buttonFinish.Location = new System.Drawing.Point(450, 4);
+            this.buttonFinish.Name = "buttonFinish";
+            this.buttonFinish.Size = new System.Drawing.Size(99, 34);
+            this.buttonFinish.TabIndex = 3;
+            this.buttonFinish.Text = "&Finish";
+            this.buttonFinish.UseVisualStyleBackColor = true;
+            this.buttonFinish.Click += new System.EventHandler(this.buttonFinish_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(6, 4);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(99, 34);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "&Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(345, 4);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(99, 34);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "&Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
             // buttonBack
             // 
@@ -84,38 +120,40 @@
             this.buttonBack.Text = "&Back";
             this.buttonBack.UseVisualStyleBackColor = true;
             // 
-            // buttonNext
+            // panelHeader
             // 
-            this.buttonNext.Location = new System.Drawing.Point(345, 4);
-            this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(99, 34);
-            this.buttonNext.TabIndex = 1;
-            this.buttonNext.Text = "&Next";
-            this.buttonNext.UseVisualStyleBackColor = true;
+            this.panelHeader.BackColor = System.Drawing.Color.SkyBlue;
+            this.panelHeader.Controls.Add(this.labelPageTitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelHeader.Location = new System.Drawing.Point(3, 3);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(551, 44);
+            this.panelHeader.TabIndex = 1;
             // 
-            // buttonCancel
+            // labelPageTitle
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(6, 4);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(99, 34);
-            this.buttonCancel.TabIndex = 2;
-            this.buttonCancel.Text = "&Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.labelPageTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPageTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelPageTitle.Location = new System.Drawing.Point(3, 3);
+            this.labelPageTitle.Name = "labelPageTitle";
+            this.labelPageTitle.Size = new System.Drawing.Size(546, 41);
+            this.labelPageTitle.TabIndex = 0;
+            this.labelPageTitle.Text = "WizardPageTitle";
+            this.labelPageTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // buttonFinish
+            // panelWizardPageContainer
             // 
-            this.buttonFinish.Location = new System.Drawing.Point(450, 4);
-            this.buttonFinish.Name = "buttonFinish";
-            this.buttonFinish.Size = new System.Drawing.Size(99, 34);
-            this.buttonFinish.TabIndex = 3;
-            this.buttonFinish.Text = "&Finish";
-            this.buttonFinish.UseVisualStyleBackColor = true;
+            this.panelWizardPageContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelWizardPageContainer.Location = new System.Drawing.Point(3, 53);
+            this.panelWizardPageContainer.Name = "panelWizardPageContainer";
+            this.panelWizardPageContainer.Size = new System.Drawing.Size(551, 327);
+            this.panelWizardPageContainer.TabIndex = 2;
             // 
             // WizardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 396);
+            this.ClientSize = new System.Drawing.Size(562, 436);
             this.Controls.Add(this.mainPanel);
             this.Name = "WizardForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -123,6 +161,7 @@
             this.mainPanel.ResumeLayout(false);
             this.wizardContentPanel.ResumeLayout(false);
             this.buttonsPanel.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -136,5 +175,8 @@
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonNext;
         private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label labelPageTitle;
+        private System.Windows.Forms.Panel panelWizardPageContainer;
     }
 }
--- ita-ui/Components/Wizard/WizardForm.cs
+++ ita-ui/Components/Wizard/WizardForm.cs
@@ -12,11 +12,118 @@ namespace iTunesAgent.UI.Components.Wizard
     public partial class WizardForm : Form
     {
 
-        private List<AbstractWizardPage> pages = new List<AbstractWizardPage>();
+        private LinkedList<AbstractWizardPage> pages = new LinkedList<AbstractWizardPage>();
+
+        private int currentPage = 0;
+
+        private WizardDataStore dataStore = new WizardDataStore();
 
         public WizardForm()
         {
             InitializeComponent();
+        }
+
+
+        public LinkedList<AbstractWizardPage> Pages
+        {
+            get { return pages; }
+        }
+
+        public WizardDataStore DataStore
+        {
+            get { return dataStore; }
+        }
+
+        /// <summary>
+        /// Start the wizard by displaying the first page.
+        /// </summary>
+        /// <param name="owner"></param>
+        public void StartWizard(IWin32Window owner)
+        {
+            ValidatePages();
+
+            NextPage();
+
+            if (owner == null)
+            {
+                ShowDialog();
+            }
+            else
+            {
+                ShowDialog(owner);
+            }
+
+
+        }
+
+        private void ApplyControlOverrideRules()
+        {
+            if (pages.Count == 1)
+            {
+                // Cancel button always enabled if only one page.
+                buttonCancel.Enabled = true;                
+            }
+
+            if (currentPage == pages.Count)
+            {
+                buttonNext.Enabled = false;
+            }
+
+            if (currentPage == 0)
+            {
+                buttonBack.Enabled = false;
+            }
+        }
+
+        private void SetControlsForPage(AbstractWizardPage page)
+        {
+            buttonCancel.Enabled = page.CancelEnabled;
+            buttonBack.Enabled = page.BackEnabled;
+            buttonNext.Enabled = page.NextEnabled;
+            buttonFinish.Enabled = page.FinishEnabled;
+        }
+
+        private void ValidatePages()
+        {
+            if (pages.Count == 0)
+            {
+                throw new NoWizardPagesException("Please add at least one before starting the wizard.");
+            }
+
+            
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (pages.ElementAt(currentPage - 1).ValidateBeforeNext())
+            {
+                NextPage();
+            }
+        }
+
+        private void NextPage()
+        {
+            AbstractWizardPage page = pages.ElementAt(currentPage++);
+            page.Populate();
+            page.DataStore = dataStore;
+            SetControlsForPage(page);
+            ApplyControlOverrideRules();
+            labelPageTitle.Text = page.PageTitle;
+            panelWizardPageContainer.Controls.Clear();
+            panelWizardPageContainer.Controls.Add(page);
+        }
+
+        private void buttonFinish_Click(object sender, EventArgs e)
+        {
+            if (pages.ElementAt(currentPage - 1).ValidateBeforeNext())
+            {
+                Close();
+            }
         }
     }
 }
