3bdac99641191f5a027b299b72d6250ecf651f7e
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-04-26 18:17:05 +0000

    Removed compiler directives.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@296 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/Components/Wizard/AbstractConditionalWizardPage.cs
+++ ita-ui/Components/Wizard/AbstractConditionalWizardPage.cs
@@ -13,20 +13,12 @@ namespace iTunesAgent.UI.Components.Wizard
     /// <summary>
     /// Abstract wizard page which is used for displaying different pages based on a condition.
     /// </summary>    
-#if DEBUG
-    // This is a hack to prevent the Visual Studio designer to crash on abstract user controls. 
-    // Ref: http://social.msdn.microsoft.com/forums/en-US/winformsdesigner/thread/ddbc181f-dda4-48ba-94b3-194494c6b1e1/
-    public partial class AbstractConditionalWizardPage : AbstractWizardPage
-#else 
-    public abstract class AbstractConditionalWizardPage : AbstractWizardPage
-#endif
+
+    public abstract partial class AbstractConditionalWizardPage : AbstractWizardPage
     {
 
-#if DEBUG
-        public virtual AbstractWizardPage GetWizardPage() { return null; }
-#else
         public abstract AbstractWizardPage GetWizardPage();
-#endif
+
 
     }
 }
