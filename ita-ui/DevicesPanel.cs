358696984b37db374fd84fa99d25ee8ee6243b98
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2011-05-06 18:32:56 +0000

    Added new wizard pages.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent2/trunk@301 3a0173bb-499f-41c5-b03c-56d250de409b

--- ita-ui/DevicesPanel.cs
+++ ita-ui/DevicesPanel.cs
@@ -9,6 +9,8 @@ using System.Windows.Forms;
 using iTunesAgent.UI.Components.Wizard;
 using iTunesAgent.UI.Properties;
 using iTunesAgent.UI.Controls;
+using iTunesAgent.Connectors;
+
 namespace iTunesAgent.UI
 {
     public partial class DevicesPanel : UserControl
@@ -45,12 +47,18 @@ namespace iTunesAgent.UI
             msdPage.PageTitle = "Configure your device";
 
             NewDeviceWPDConfigurationPage wpdPage = new NewDeviceWPDConfigurationPage();
+            wpdPage.DevicesService = new WindowsPortableDevicesService();
             wpdPage.PageTitle = "Configure your device";
 
             NewDeviceTypeBasedConditionalPage deviceConfigurationPage = new NewDeviceTypeBasedConditionalPage();
             deviceConfigurationPage.MassStorageDevicePage = msdPage;
             deviceConfigurationPage.WindowsPortableDevicePage = wpdPage;
             wizard.Pages.AddLast(deviceConfigurationPage);
+
+            NewDeviceSummaryPage summaryPage = new NewDeviceSummaryPage();
+            summaryPage.PageTitle = "Summary";
+
+            wizard.Pages.AddLast(summaryPage);
 
 
             wizard.StartWizard(this);
