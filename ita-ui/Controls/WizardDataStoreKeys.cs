aaeca6bf2bb2062ed15ad17d975c92ec01dd8bd9
Jaran Nilsen <jaran.nilsen@gmail.com>
2011-06-11 17:11:22 +0200

    Added static values for data store objects

---
+++ ita-ui/Controls/WizardDataStoreKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunesAgent.UI.Controls
+{
+    public abstract  class WizardDataStoreKeys
+    {
+        public static readonly String DEVICE = "wpdDevice";
+        public static readonly String NAME = "deviceName";
+        public static readonly String OPEN_PLAYLIST_PANEL = "openPlaylistPanelOnFinish";
+
+    }
+ }
