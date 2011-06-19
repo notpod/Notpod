07745fed51548a82a2e5b3b0bd23c26c76debb3d
Jaran Nilsen <jaran.nilsen@gmail.com>
2011-06-19 17:43:19 +0200

    Added new mock implementations.

---
+++ ita-itunes-test/MockLibrarySource.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+namespace iTunesAgent.Services
+{
+    public class MockLibrarySource : IITSource 
+    {
+
+        private IITPlaylistCollection playlists;
+
+        public double Capacity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double FreeSpace
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void GetITObjectIDs(out int sourceID, out int playlistID, out int trackID, out int databaseID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Index
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ITSourceKind Kind
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IITPlaylistCollection Playlists
+        {
+            get { return playlists; }
+            set { this.playlists = value; }
+        }
+
+        public int TrackDatabaseID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int playlistID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int sourceID
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int trackID
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ ita-itunes-test/MockPlaylistCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+
+namespace iTunesAgent.Services
+{
+    public class MockPlaylistCollection : IITPlaylistCollection
+    {
+
+        public List<IITPlaylist> playlists;
+
+        public List<IITPlaylist> Playlists
+        {
+            get { return this.playlists; }
+            set { this.playlists = value; }
+        }
+
+        public int Count
+        {
+            get { return this.playlists.Count; }
+        }
+
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            return playlists.GetEnumerator();
+        }
+
+        public IITPlaylist get_ItemByName(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IITPlaylist get_ItemByPersistentID(int highID, int lowID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IITPlaylist this[int index]
+        {
+            get { return this.playlists[index]; }
+        }
+    }
+}
