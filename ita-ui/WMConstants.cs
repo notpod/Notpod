34069ac6cda900ed30d87cebcc6163b20645f5b5
Jaran Nilsen <jaran.nilsen@gmail.com>
2011-07-28 14:28:09 +0200

    added new class containing constants for windows messages.

---
+++ ita-ui/WMConstants.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jaran
+ * Date: 28.07.2011
+ * Time: 13:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace iTunesAgent.UI
+{
+	/// <summary>
+	/// Constants used when intercepting system messages
+	/// </summary>
+	public sealed class WMConstants
+	{
+		
+        public const int WM_SYSCOMMAND = 0x112;
+        public const int SC_MINIMIZE = 0xF020;
+        
+        public const int WM_DEVICECHANGE = 0x0219;        
+        public const int DBT_DEVICEARRIVAL = 0x8000;        
+        public const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+	}
+}
