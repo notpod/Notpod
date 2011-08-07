962a896a186a71b271f5d8fb5c83818b56db1a4f
Jaran Nilsen <jaran.nilsen@gmail.com>
2011-08-07 17:08:25 +0200

    Initial import of panel for managing playlist associations.

---
+++ ita-ui/AssociationManagementPanel.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jaran
+ * Date: 07.08.2011
+ * Time: 16:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace iTunesAgent.UI
+{
+    /// <summary>
+    /// Description of AssociationManagementPanel.
+    /// </summary>
+    public partial class AssociationManagementPanel : UserControl
+    {
+        public AssociationManagementPanel()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+    }
+}
