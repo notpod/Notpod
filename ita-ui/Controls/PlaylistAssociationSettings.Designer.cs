8d9eba8a86a27de8e6053e96de629710e617d658
Jaran Nilsen <jaran.nilsen@gmail.com>
2011-08-20 14:54:47 +0200

    initial import.

---
+++ ita-ui/Controls/PlaylistAssociationSettings.Designer.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jaran
+ * Date: 26.07.2011
+ * Time: 15:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace iTunesAgent.UI.Controls
+{
+    partial class PlaylistAssociationBrowseFolder
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Disposes resources used by the control.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+        	this.lblInfo = new System.Windows.Forms.Label();
+        	this.btnBrowse = new System.Windows.Forms.Button();
+        	this.lblSelectedDestination = new System.Windows.Forms.Label();
+        	this.SuspendLayout();
+        	// 
+        	// lblInfo
+        	// 
+        	this.lblInfo.Location = new System.Drawing.Point(4, 4);
+        	this.lblInfo.Name = "lblInfo";
+        	this.lblInfo.Size = new System.Drawing.Size(505, 21);
+        	this.lblInfo.TabIndex = 0;
+        	this.lblInfo.Text = "Please choose the folder on your device where you want the music copied to:";
+        	// 
+        	// btnBrowse
+        	// 
+        	this.btnBrowse.Location = new System.Drawing.Point(101, 44);
+        	this.btnBrowse.Name = "btnBrowse";
+        	this.btnBrowse.Size = new System.Drawing.Size(303, 51);
+        	this.btnBrowse.TabIndex = 1;
+        	this.btnBrowse.Text = "Browse";
+        	this.btnBrowse.UseVisualStyleBackColor = true;
+        	this.btnBrowse.Click += new System.EventHandler(this.BtnBrowseClick);
+        	// 
+        	// lblSelectedDestination
+        	// 
+        	this.lblSelectedDestination.Location = new System.Drawing.Point(4, 125);
+        	this.lblSelectedDestination.Name = "lblSelectedDestination";
+        	this.lblSelectedDestination.Size = new System.Drawing.Size(505, 92);
+        	this.lblSelectedDestination.TabIndex = 2;
+        	// 
+        	// PlaylistAssociationBrowseFolder
+        	// 
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.Controls.Add(this.lblSelectedDestination);
+        	this.Controls.Add(this.btnBrowse);
+        	this.Controls.Add(this.lblInfo);
+        	this.Name = "PlaylistAssociationBrowseFolder";
+        	this.Size = new System.Drawing.Size(512, 318);
+        	this.ResumeLayout(false);
+        }
+        private System.Windows.Forms.Label lblSelectedDestination;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ ita-ui/Controls/PlaylistAssociationSettings.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jaran
+ * Date: 26.07.2011
+ * Time: 15:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using iTunesAgent.UI.Components.Wizard;
+using iTunesAgent.Domain;
+using iTunesAgent.UI.Properties;
+using iTunesAgent.Connectors;
+using iTunesAgent.Connectors.Domain;
+
+namespace iTunesAgent.UI.Controls
+{
+    /// <summary>
+    /// Description of PlaylistAssociationBrowseFolder.
+    /// </summary>
+    public partial class PlaylistAssociationBrowseFolder : AbstractWizardPage
+    {
+        
+        private IDevicesManager devicesManager;
+        
+        
+        public PlaylistAssociationBrowseFolder()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+        
+        
+        public override void Populate()
+        {
+            ResetSelectedPath();
+            
+        }
+        
+        private void ResetSelectedPath()
+        {
+            // We ensure the selected path is reset.
+            this.DataStore[WizardDataStoreKeys.PLAYLIST_ASSOCIATION_SELECTEDPATH] = null;
+            lblSelectedDestination.Text = "";
+        }
+        
+        public override bool ValidateBeforeNext()
+        {
+            return true;
+        }
+        
+        void BtnBrowseClick(object sender, EventArgs e)
+        {
+            Device selectedDevice = (Device)DataStore[WizardDataStoreKeys.PLAYLIST_ASSOCIATION_SELECTEDDEVICE];
+            
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            
+            folderBrowser.RootFolder = Environment.SpecialFolder.MyComputer;
+            
+            // TODO Need to set root folder of the browser to the root of the selected device.
+            
+            DialogResult result = folderBrowser.ShowDialog(this);
+            if(result == DialogResult.OK) {
+                
+                
+                CompatibleDevice device = devicesManager.GetConnectedDevice(selectedDevice.Identifier);
+                bool folderExistsOnDevice = device.CheckPath(folderBrowser.SelectedPath);
+                
+                if(!folderExistsOnDevice)
+                {
+                    
+                    MessageBox.Show(this, String.Format("The folder you selected ({0}) was not found on the device '{1}'. Are you sure you browsed the correct device?",
+                                                        folderBrowser.SelectedPath, selectedDevice.Name), "Unable to verify folder location",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                    
+                }
+                
+                this.DataStore[WizardDataStoreKeys.PLAYLIST_ASSOCIATION_SELECTEDPATH] = folderBrowser.SelectedPath;
+                lblSelectedDestination.Text = String.Format("{0}:\n{1}", Resources.StrPlaylistAssociationCurrentlySelected, folderBrowser.SelectedPath);
+                
+            }
+            
+        }
+        
+        public IDevicesManager DevicesManager {
+            get { return devicesManager; }
+            set { devicesManager = value; }
+        }
+
+    }
+}
